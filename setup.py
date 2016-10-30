d18085a2e9d452e1f22152b9446817c530c788f5
Ronald Ilma <rri5@cornell.edu>
2016-10-30 15:58:10 -0400

    Add files via upload

--- setup.py
+++ setup.py
@@ -38,14 +38,17 @@ for tmp in ['apexqd2gd.pyf', 'qd2gdcoord.f90', 'apexsh.f90']:
 ext3 = Extension( name='apexqd2gd', \
 	sources=sources3, \
 	extra_f77_compile_args=f77CompileArgs \
-        )
-
+       )
 
 if __name__ == '__main__':
 
-    setup( author='Ronald Ilma', \
+    setup( name='pyapex', \
+        version='0.0.1', \
+        author='Ronald Ilma', \
         author_email='rri5@cornell.edu', \
         description='Apex Model Apps', \
+        packages=['pyapex'], \
+        ext_package='pyapex', \
         ext_modules=[ ext1, ext3 ], \
-	data_files=[('', ['apexsh.dat'])], \
+        data_files=[('pyapex', [sourcePath + 'apexsh.dat'])], \
         )
