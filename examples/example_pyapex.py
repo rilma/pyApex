b83408650d02a678290aeea5d03e856afae992a9
rilma <rri5@cornell.edu>
2016-11-30 13:39:52 -0500

    Field-line example

--- examples/example_pyapex.py
+++ examples/example_pyapex.py
@@ -1,4 +1,5 @@
 
+import seaborn
 from matplotlib.pyplot import figure, show
 import pyapex
 from scipy import arange, nan, where
@@ -9,7 +10,8 @@ if __name__ == '__main__':
 
         Obj = pyapex.ApexFL()
 
-        date = 2003.
+        year, doy = 2003, 323
+        date = 2003. + doy / 365
         dlon = -75.
         hlim = [80., 160.]
 
@@ -27,6 +29,13 @@ if __name__ == '__main__':
             if len(ind) > 0: x[ind], y[ind], z[ind] = nan, nan, nan
             pn.plot(x, y)
 
+        strDate = 'DATE: {:4d}.{:03d}'.format(year, doy)
+        #strLoc = 'GEOG. LON.: {:6.2f}$^\circ$'.format(dlon)
+        strLoc = 'GEOG. LON.: {:6.2f}$^\circ${:s}'.format(abs(dlon), 'E' if dlon > 0. else 'W') 
+        title = '{:s}    -    {:s}'.format(strDate, strLoc)
+        pn.set_title(title)
+        pn.set_xlabel("Geog. Lat. ($^\circ$)")
+        pn.set_ylabel("Altitude (km)")
         pn.invert_xaxis()
         show()
 
